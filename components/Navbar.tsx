d176090511a1797513561905c27b065448cf1d99
muhananaufal <muhananaufal8@gmail.com>
2024-10-08 12:44:22 +0700

    auth and dashboard

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { ThemeToggle } from './ThemeToggle';
+import { Button } from './ui/button';
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserNav } from './UserNav';
+
+
+export async function Navbar() {
+	const {isAuthenticated, getUser} = getKindeServerSession()
+	const user = await getUser()
+	
+	return (
+
+		
+		<nav className="border-b bg-background h-[10vh] flex justify-center">
+			<div className="container flex items-center justify-between">
+				<Link href="/">
+					<h1 className="font-bold text-3xl">Simple<span className='text-primary'>Notes</span></h1>
+				</Link>
+				<div className="flex items-center gap-x-5">
+					<ThemeToggle />
+
+		{await isAuthenticated()? (
+			<UserNav name={user?.given_name as string} email={user?.email as string} image={user?.picture as string}/>
+			
+			// <LogoutLink>
+			// 	<Button>Log Out</Button>
+
+			// </LogoutLink>
+		) : (          
+		<div className='flex items-center gap-x-5'>
+			<LoginLink>
+				<Button>Sign In</Button>
+
+			</LoginLink>
+			<RegisterLink>
+				<Button variant='secondary'>Sign Up</Button>
+
+			</RegisterLink>
+
+			</div>)}
+				</div>
+			</div>
+		</nav>
+	);
+}
